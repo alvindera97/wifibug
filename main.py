d7a0ef5fdb77dd0e6a44f4da462898272dcfe7ad
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2023-01-02 13:49:14 +0100

    Reformat File To Comply With PEP8

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import sys
-from typing import NoReturn, Dict, Callable, Any
+from typing import NoReturn, Dict, Callable, Optional
 
-
-EXECUTE: Dict[int | None, Callable[[], Any] | Callable[[], Any] | Callable[[], None]] = {
-    2: lambda: main(headless=True if sys.argv[1] == "headless" else False),
-    4: lambda: main(headless=True if sys.argv[1] == "headless" else False, username=sys.argv[2], password=sys.argv[3]),
-    None: lambda: print(f"USAGE: python main.py [headless/visual] [username password]")
+EXECUTE: Dict[Optional[int], Callable] = {
+    2: lambda: main(
+        headless=True if sys.argv[1] == "headless" else False),
+    4: lambda: main(
+        headless=True if sys.argv[1] == "headless" else False,
+        username=sys.argv[2],
+        password=sys.argv[3]),
+    None: lambda: print(
+        f"USAGE: python main.py [headless/visual] [username password]")
 }
 
 
-class NoUsernamePasswordSetError(Exception): pass
+class NoUsernamePasswordSetError(Exception):
+    pass
 
 
-def main(headless: bool = False, username: str = None, password: str = None) -> NoReturn:
+def main(
+        headless: bool = False,
+        username: str = None,
+        password: str = None) -> NoReturn:
     """
     Persistently try to login to UNIBEN wifi given username and password.
 
-    If username and password is given, then given username and password combinations are used login,
-    if given username and password combinations are incorrect however, main() fails instantly displaying the error
+    If username and password is given,
+    the given username and password combinations are used login,
+    if given username and password combinations are incorrect however,
+     main() fails instantly displaying the error
 
-    If username and password aren't given, environment variables storing "UNIBEN_WIFI_USERNAME' and 'UNIBEN_WIFI_PASSWORD'
-    are used as credentials.
-    If no environment variable for 'UNIBEN_WIFI_USERNAME' and [or] 'UNIBEN_WIFI_PASSWORD' exist, main() raises NoUsernamePasswordSetError
-    It's following operation is the same as if username and password were originally given.
+    If username and password aren't given,
+    environment variables storing "UNIBEN_WIFI_USERNAME' and
+    'UNIBEN_WIFI_PASSWORD' are used as credentials.
+    If no environment variable for 'UNIBEN_WIFI_USERNAME'
+    and [or] 'UNIBEN_WIFI_PASSWORD' exist,
+    main() raises NoUsernamePasswordSetError
+
+    It's following operation is the same as if username
+    and password were originally given.
     """
-    pass
 
 
 if __name__ == "__main__":
